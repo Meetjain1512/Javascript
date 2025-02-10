e2e7beec0a2f7e33f303233d901cfe108efec1eb
Meetjain1512 <99678497+Meetjain1512@users.noreply.github.com>
2025-02-10 17:56:35 +0000

    done

---
+++ 1/datatype.js
@@ -0,0 +1,25 @@
+"use strict" // treat all below written js code as new version
+// alert("hello") // uses node.js not browser
+
+console.log(3+3)
+let name = "Harry"
+let age = 18
+let isLoggedIn = false
+let a
+let b = "True"
+let state = null
+console.table([name, age, a, b, state])
+
+// Premitive datatypes
+    // number => 2 to power 53
+    // bigint
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined => 
+    // symbol => unique
+
+// object
+
+console.log(typeof age);
+console.log(typeof null); // object
