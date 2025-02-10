3d1ad06c58c415175af68b855b24e1951a30d475
Meetjain1512 <99678497+Meetjain1512@users.noreply.github.com>
2025-02-10 16:26:10 +0000

    done

---
+++ 1/variables.js
@@ -0,0 +1,10 @@
+const accountId = 746
+let accountemail = "meet@gmail.com"
+var accountpassowrd = "12345"
+accoutncity = "Mumbai"
+accountemail = "mW@gmail.com"
+accountpassowrd = "1212"
+// accountId = 2
+console.log(accountId)
+console.log(accountId, accountemail, accoutncity)
+console.table([accountId, accountemail,accountpassowrd, accoutncity])
