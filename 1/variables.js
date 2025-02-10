0feee180177f4836b30b611ff6f014fcc63c3a23
Meetjain1512 <99678497+Meetjain1512@users.noreply.github.com>
2025-02-10 16:28:55 +0000

    done

--- 1/variables.js
+++ 1/variables.js
@@ -5,6 +5,10 @@ accoutncity = "Mumbai"
 accountemail = "mW@gmail.com"
 accountpassowrd = "1212"
 // accountId = 2
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
 console.log(accountId)
 console.log(accountId, accountemail, accoutncity)
 console.table([accountId, accountemail,accountpassowrd, accoutncity])
