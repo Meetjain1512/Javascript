71d61aadd046693bb5781a5802fe1f499d2ae7ca
Meetjain1512 <99678497+Meetjain1512@users.noreply.github.com>
2025-02-11 06:57:51 +0000

    done

---
+++ 1/conversion_operations.js
@@ -0,0 +1,41 @@
+let score = "25abc"
+console.log(typeof score)
+// console.log(typeof (score))
+
+let ValueInnumber = Number(score)
+console.log(typeof ValueInnumber)
+console.log(ValueInnumber)
+
+let score1 = null
+console.log(typeof score)
+// console.log(typeof (score))
+
+let ValueInnumber1 = Number(score1)
+console.log(typeof ValueInnumber)
+console.log(ValueInnumber1)
+
+// *******Operations********
+
+let val = 10
+let negvalue = -val
+console.log(negvalue)
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = "World"
+let str3 = str1 + str2
+// console.log(str1 + str2)
+// console.log("1" + 2)
+// console.log(2+2+"1")
+// console.log(2+"1"+2)
+// console.log((2+2)+"1"+2)
+// console.log(+false)
+
+let counter = 100
+counter++
+console.log(counter)
